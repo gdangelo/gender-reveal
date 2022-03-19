227fbafec26a089610cf48b49ba55e17b92fe384
Grégory D'Angelo <dangelo.gregory@gmail.com>
2022-03-19 09:56:51 +0100

    chore: Create Next.js app + install Tailwind CSS

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Gender Reveal</title>
+        <meta name="description" content="Gender Reveal" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+    </div>
+  );
+}
