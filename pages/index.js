384f8509b0161a5b99228e19b1f03e36417dc8fd
Grégory D'Angelo <dangelo.gregory@gmail.com>
2022-03-19 16:49:31 +0100

    feat: Add preload auto

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
 
   return (
     <div>
-      <audio ref={audioRef} controls loop className="hidden">
+      <audio ref={audioRef} preload="auto" loop className="hidden">
         <source src="heartbeat.mp3" type="audio/mp3" />
         <source src="heartbeat.ogg" type="audio/ogg" />
       </audio>
